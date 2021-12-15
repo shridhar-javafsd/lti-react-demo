2bf997feefc1bfe2eb0d15d085fc6055c21351f7
D Shridhar <shridhar.javafsd@gmail.com>
2021-12-15 12:30:27 +0530

    Update Emp.js

--- src/components/Emp.js
+++ src/components/Emp.js
@@ -1,9 +1,21 @@
 
-const Emp = () => {
+// state - a object to store data of the component 
+// React 16.8 onwards, state is available in function components also 
+// react provides hooks (methods) to use state 
+
+import { useState } from "react";
+
+const Emp = (props) => {
+
+    const [emp, setEmp] = useState({});
+
+    // const emp = { eid: 101, firstName: 'Sonu', salary: 48000 };
 
     return (
         <div>
             <p>Emp Component</p>
+            <h1>Employee Data</h1>
+            <p> {emp.eid} {emp.firstName} {emp.salary} </p>
         </div>
 
     );
