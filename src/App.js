c196391cb8da8ea7516edd8e8168d523a35c7c51
D Shridhar <shridhar.javafsd@gmail.com>
2021-12-15 10:12:03 +0530

    Commit

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ const App = () => {
     </div>
   );
 }
+
 export default App;
 
 // function App() {
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+
+const Header = () => {
+
+    return (
+        <div>
+            <p>This is a header</p>
+        </div>
+    );
+
+}
+
+export default Header;
