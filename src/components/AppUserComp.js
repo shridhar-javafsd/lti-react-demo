fcf95924c9572dbefd4b9ad003aa0c6dc0e0386c
D Shridhar <shridhar.javafsd@gmail.com>
2021-12-16 17:24:45 +0530

    Update AppUserComp.js

--- src/components/AppUserComp.js
+++ src/components/AppUserComp.js
@@ -1,10 +1,9 @@
-
 import axios from "axios";
 import { useState } from "react";
 
 const AppUserComp = () => {
 
-    const [appuser, setAppUser] = useState({});
+    const [appUser, setAppUser] = useState({});
     const [uid, setUid] = useState('');
 
     const handleUidInput = (evt) => {
@@ -13,13 +12,13 @@ const AppUserComp = () => {
 
     const submitGetAppUserById = (evt) => {
         console.log('submitGetAppUserById');
-        // axios.get('http://localhost:8082/emp/getempbyid/4')
-        // axios.get('https://jsonplaceholder.typicode.com/users/4')
         axios.get('https://jsonplaceholder.typicode.com/users/' + uid)
             .then((response) => {
                 console.log(response.data);
             })
-            .catch();
+            .catch((error) => {
+                console.log(error.message);
+            });
         evt.preventDefault();
     }
 
@@ -43,3 +42,48 @@ const AppUserComp = () => {
 }
 
 export default AppUserComp;
+
+// import axios from "axios";
+// import { useState } from "react";
+
+// const AppUserComp = () => {
+
+//     const [appuser, setAppUser] = useState({});
+//     const [uid, setUid] = useState('');
+
+//     const handleUidInput = (evt) => {
+//         setUid(evt.target.value);
+//     }
+
+//     const submitGetAppUserById = (evt) => {
+//         console.log('submitGetAppUserById');
+//         // axios.get('http://localhost:8082/emp/getempbyid/4')
+//         // axios.get('https://jsonplaceholder.typicode.com/users/4')
+//         axios.get('https://jsonplaceholder.typicode.com/users/' + uid)
+//             .then((response) => {
+//                 console.log(response.data);
+//             })
+//             .catch();
+//         evt.preventDefault();
+//     }
+
+//     return (
+//         <div className="container">
+//             <p className="display-4 text-primary">App User Component</p>
+//             <div>
+//                 <p>AppUser Data: </p>
+//             </div>
+//             <div className="col-4">
+//                 <form className="form form-group form-primary">
+//                     <input className="form-control mt-3" type="number" id="uid" name="uid" value={uid}
+//                         onChange={handleUidInput} placeholder="Please enter userid"></input>
+//                     <input className="form-control mt-3 btn btn-primary" type="submit" id="submit" name="submit" value="Get App User"
+//                         onClick={submitGetAppUserById}></input>
+//                 </form>
+//             </div>
+//         </div>
+//     );
+
+// }
+
+// export default AppUserComp;
