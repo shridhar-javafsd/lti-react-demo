569921b91ccf310e1b7e8e4d6176b9d486fa9832
D Shridhar <shridhar.javafsd@gmail.com>
2021-12-17 13:59:01 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,27 @@
+import { Link } from "react-router-dom";
 
 const Header = () => {
 
     return (
         <header className="header">
             <nav className="navbar bg-dark">
-            </nav>
+                <div>
+                    <ul className="navbar-nav ml-auto">
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/emp">Emp</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/appuser">AppUser</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/dept">Dept</Link>
+                        </li>
 
+                    </ul>
+                </div>
+
+
+            </nav>
         </header>
 
     );
