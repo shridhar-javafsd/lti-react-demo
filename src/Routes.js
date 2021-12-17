48676091cd81819cef7857e0d5812528bc0b495c
D Shridhar <shridhar.javafsd@gmail.com>
2021-12-17 11:54:47 +0530

    Update Routes.js

--- src/Routes.js
+++ src/Routes.js
@@ -1,3 +1,10 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Header from './components/Header';
+import Emp from './components/Emp';
+import AppUserComp from './components/AppUserComp';
+import Dept from './components/Dept';
+import Footer from './components/Footer';
+import MyMedia from './components/MyMedia';
 
 
 const Routes = () => {
@@ -5,6 +12,20 @@ const Routes = () => {
     return (
         <div>
             <p>Routes Component</p>
+            <div>
+                <Router>
+                    <div>
+                        <Header />
+                        <div>
+                            <Switch>
+                                <Route path="/emp" > <Emp /> </Route>
+                                <Route path="/appuser" > <AppUserComp /> </Route>
+                                <Route path="/media" > <MyMedia /> </Route>
+                            </Switch>
+                        </div>
+                    </div>
+                </Router>
+            </div>
         </div>
     );
 
