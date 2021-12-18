62251f494310b3cd33b5ad459ba16fade8c85368
D Shridhar <shridhar.javafsd@gmail.com>
2021-12-18 10:21:48 +0530

    Update AppUserSlice.js

--- src/redux/AppUserSlice.js
+++ src/redux/AppUserSlice.js
@@ -1,6 +1,25 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const AppUserSlice = createSlice();
+const AppUserSlice = createSlice({
+
+    name: 'appUser',
+
+    initialState: {
+        
+        appUserState: {}
+
+    },
+
+    reducers: {
+        getAppUserById: (state, action) => {
+            console.log(`AppUserSlice getAppUserById`);
+            state.appUserState = action.payload;
+        }
+        // , other methods similar to this 
+    }
+
+});
+
+export const { getAppUserById } = AppUserSlice.actions;
 
 export default AppUserSlice.reducer;
-
