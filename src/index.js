56bc648d4879484fc33dfb7b593fb629a862d12c
D Shridhar <shridhar.javafsd@gmail.com>
2021-12-18 10:12:20 +0530

    C

--- src/index.js
+++ src/index.js
@@ -3,14 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 // ReactDOM.render(arg1, arg2);
 
+// with redux store 
 ReactDOM.render(
-  <App />
+  <Provider store={store}>
+    <App />
+  </Provider>
   ,
   document.getElementById('root')
 );
+
+
+// // without redux store 
+// ReactDOM.render(
+//   <App />
+//   ,
+//   document.getElementById('root')
+// );
 
 
 
