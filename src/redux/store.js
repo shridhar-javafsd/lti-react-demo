b602e9dc1880ce5400390376b4867ab2cfc7324c
D Shridhar <shridhar.javafsd@gmail.com>
2021-12-18 10:04:57 +0530

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,10 @@
-
 import { configureStore } from "@reduxjs/toolkit";
+
+// steps to create and use redux store - 
+// 1. create store - only one 
+// 2. provide store to index.js 
+// 3. create slices for each components 
+// 4. use slices in the components 
 
 const store = configureStore(
     {
