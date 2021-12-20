a40a633a7b9e2291186f79711c788e1cd136306d
D Shridhar <shridhar.javafsd@gmail.com>
2021-12-20 18:01:09 +0530

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,36 @@
+// import App from './App';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import { Provider } from 'react-redux';
+import Dept from './components/Dept';
+import store from './redux/store';
+import AppUserComp from './components/AppUserComp';
+// syntax test('', () => {})
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('test dept component', () => {
+  render(
+    <Provider store={store}>
+      <Dept />
+    </Provider>
+  );
+  const dataFromDept = screen.getByText('Dept1 Component');
+  expect(dataFromDept).toBeInTheDocument();
 });
+
+test('test AppUser component', () => {
+  render(
+    <Provider store={store}>
+      <AppUserComp />
+    </Provider>
+  );
+  const dataFromDept = screen.getByText('get App User by id:');
+  expect(dataFromDept).toBeInTheDocument();
+});
+
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
+
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
